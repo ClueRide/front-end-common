699e9c4a8332346536884bd6e3caa41bc02e7947
jett <jettmarks@gmail.com>
2018-12-13 21:57:17 -0500

    FEC-27 Registration State resource
    - Adds RegStateService which knows how to talk to the REST API.
    
    Ticket FEC-29 is a follow on which will sort out various issues with
    the current procedure for registering based on what was learned from
    making the connection to the backend as prepared in this commit.

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -4,17 +4,18 @@ import {IonicStorageModule} from "@ionic/storage";
 import {AuthService} from "../providers/auth/auth.service";
 import {BadgeService} from "../providers/badge/badge.service";
 import {ConfirmPageModule} from "../pages/confirm/confirm.module";
+import {DeviceGeoLocService} from "../providers/device-geo-loc/device-geo-loc.service";
+import {GeoLocService} from "../providers/geo-loc/geo-loc";
 import {HttpService} from "../providers/http/http.service";
 import {InviteService} from "../providers/invite/invite.service";
+import {LatLonProvider} from "../providers/lat-lon/lat-lon";
 import {ProfileService} from "../providers/profile/profile.service";
 import {RegistrationPageModule} from "../pages/registration/registration.module";
+import {RegStateService} from "../providers/reg-state/reg-state.service";
 import {Resource} from "../providers/resources/resource";
 import {RestangularModule} from "ngx-restangular";
 import {RestangularConfigFactory} from "../providers/resources/resource.config";
 import {TokenService} from "../providers/token/token.service";
-import {GeoLocService} from "../providers/geo-loc/geo-loc";
-import {DeviceGeoLocService} from "../providers/device-geo-loc/device-geo-loc.service";
-import {LatLonProvider} from "../providers/lat-lon/lat-lon";
 
 @NgModule({
   imports: [
@@ -40,6 +41,7 @@ export class ComponentsModule {
         InviteService,
         LatLonProvider,
         ProfileService,
+        RegStateService,
         TokenService
       ]
     }
