777e38bb5c39b0fd43386826176315c772ee34c6
jett <jettmarks@gmail.com>
2019-02-23 21:31:47 -0500

    LE-99 Move off Restangular to HttpClient
    - Support for removing Restangular from MobiLoc; some functionality
    came here.

--- src/providers/location/location.ts
+++ src/providers/location/location.ts
@@ -1,18 +1,31 @@
+import {Injectable} from "@angular/core";
 import {LatLon} from "../lat-lon/lat-lon";
 
+/**
+ * Created by jett on 7/10/17.
+ */
 export interface Image {
   id: number;
   url: string;
 }
 
+@Injectable()
 export class Location {
-  id: number;
-  name: string;
-  description: string;
-  latLon: LatLon;
-  featuredImage: Image;
 
-  /* GeoJSON representation for placement on a Map. */
-  geoJson: {};
+  id: number;
+  nodeId: number;   // Provides Lat/Lon
+  latLon: LatLon;
+  name?: string;
+  description?: string;
+  locationGroupId?: number;
+  locationTypeName?: string;
+  locationTypeIconName?: string;
+  locationTypeId: number;
+  featuredImageUrl?: string;
+  featuredImage: Image;
+  establishmentId?: number;
+  readinessLevel: string;
 
 }
+
+
