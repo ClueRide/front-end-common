81a73e068a2308420444d3613dbeb25fa5b80435
jett <jettmarks@gmail.com>
2019-03-07 20:04:24 -0500

    Bug: Inability to connect leads to registration page
    - Emits the NO_NETWORK_CONNECTION AuthState when we can't connect to the back-end.

--- src/providers/auth/auth-state.ts
+++ src/providers/auth/auth-state.ts
@@ -6,6 +6,7 @@
  * a given device.
  */
 export enum AuthState {
+  NO_NETWORK_CONNECTION,  // Unable to connect to back-end to perform check.
   UNREGISTERED,   // Never before registered or requiring a new registration
   REGISTERED,     // Valid registration that has not expired
   EXPIRED         // Valid registration that has expired and needs to be renewed
